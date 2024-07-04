2af579e73e67fc22f2c43872831b75d494c717d9
pdomaleczny <patryk.domaleczny@gmail.com>
2024-07-04 12:33:19 +0200

    Remove frontend as submodule from git configuration

---
+++ fms-frontend/next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
---
+++ fms-frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">⚽ Football matches simulator ⚽</h1>
+      Test
+    </main>
+  );
+}
